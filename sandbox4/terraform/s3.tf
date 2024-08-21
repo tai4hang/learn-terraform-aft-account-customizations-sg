e7ce5e5c8227f0d087d1207536a2f3a5501f8c92
Gary Chan <gary.chan@qualicom.com>
2024-08-21 12:12:49 -0400

    update s3 name

--- sandbox4/terraform/s3.tf
+++ sandbox4/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox4-iam-${data.aws_caller_identity.current.account_id}"
+  bucket = "aft-sandbox4-sg-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
